94bf7de3beb943df2a6b157f39644484ccaf4775
tuj45886 <58963931+tuj45886@users.noreply.github.com>
2023-02-12 01:08:25 -0500

    Skeleton for word class

---
+++ src/com/zetcode/Word.java
@@ -0,0 +1,21 @@
+package com.zetcode;
+
+public class Word {
+    String word = "";
+    public Word(String word){
+        this.word = word;
+    }
+
+    public String getWord(){
+        //get a random word fromm the file
+        return "";
+    }
+
+    public String confuse(){
+        //use mapping of commonly confused letters to
+        // generate a "confused" version of the current word
+        return null;
+    }
+
+
+}
